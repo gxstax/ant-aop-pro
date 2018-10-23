6553330d61f970fa5358dcad612a21a98fc6e948
gaoxx <gaoxx@fxiaoke.com>
2018-10-23 10:25:53 +0800

    代码提交测试

---
+++ src/main/java/com/ant/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.ant.config;
+
+/**
+ * @author gaoxx gaoxx@fxiaoke.com
+ * @ClassName: AppConfig
+ * @Description: TODO
+ * @datetime 2018/10/23 10:25
+ * @Version 1.0
+ */
+public class AppConfig {
+}
