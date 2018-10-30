c4930c60e18bfabca89ce94096175581ee1fd495
gaoxx <gaoxx@fxiaoke.com>
2018-10-30 13:23:30 +0800

    aop-spring提交

---
+++ spring-aop/src/test/java/com/ant/test/HashMapTest.java
@@ -0,0 +1,22 @@
+package com.ant.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author gaoxx gaoxx@fxiaoke.com
+ * @ClassName: HashMapTest
+ * @Description: TODO
+ * @datetime 2018/10/23 11:06
+ * @Version 1.0
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map<String, String> temHsMap = new HashMap<>();
+        temHsMap.put("1", "First");
+
+        System.out.println(temHsMap.isEmpty());
+
+        System.out.println("dsa");
+    }
+}
